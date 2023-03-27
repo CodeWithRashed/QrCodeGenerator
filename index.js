ee0281ed97654b20098f99d751252b9df5a68732
deve-rashed <tools.rashed@gmail.com>
2023-03-27 14:53:19 +0600

    remove alert and added shake effect for empty input

--- index.js
+++ index.js
@@ -5,12 +5,16 @@ var imgbox = document.getElementById('imagebox');
 
 function generateQr(){
 
-    if (inputText.value.length> 0) {
+    if (inputText.value.length > 0) {
         var qrcode = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + inputText.value;
         qrImg.src = qrcode;
         imgbox.classList.add("imageboxafter");
         document.getElementById('card').classList.add("cardafter");
-    } else{
-        alert("Please Enter Text or URL")
-    }
+      } else {
+        document.getElementById("inputtext").classList.add("shake");
+        setTimeout(function() {
+          document.getElementById("inputtext").classList.remove("shake");
+        }, 1000); 
+      }
+      
 };
